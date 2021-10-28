92c778070f5a39fcd7fcf8c82f27d63a48d6a741
Priscilla Robinson <priscilla.wojtowicz@gmail.com>
2021-10-27 19:36:09 -0500

    add files

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ engine = create_engine("sqlite:///DataFiles/SAEXP1")
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    result = engine.execute("select * from us_expenditures")
+    rows = result.fetchall()
+    result_list = []
+    for r in rows:
+        result_list.append(dict(r))
+
+    return jsonify(result_list)
+
 @app.route("/api/v1.0")
 def all_data():
     result = engine.execute("select * from us_expenditures")
