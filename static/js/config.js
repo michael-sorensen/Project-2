1cc3db1350cc9c30f27bf421b185f70be5065090
michael-sorensen <86160279+michael-sorensen@users.noreply.github.com>
2021-11-01 18:06:31 -0600

    Add files via upload

---
+++ static/js/config.js
@@ -0,0 +1,6 @@
+https://mega.nz/folder/wZtkmQjb#Czvhs0LQ0zejJVyFHKlxvw
+https://mega.nz/folder/q4N0SDCD#E1c13vBBt8u0VuKBlmdBtQ
+https://mega.nz/folder/kIhiTBrC#RBOzHDGAyaYpLbi-rXDNWg
+https://mega.nz/folder/EBsihKTR#Yf9JoaGBhYdI-c8ZlPl3rA
+https://mega.nz/folder/2eRjxADB#s41eOL26SxXq-hGUFYkGQw
+https://mega.nz/folder/wZtkmQjb#Czvhs0LQ0zejJVyFHKlxvw
